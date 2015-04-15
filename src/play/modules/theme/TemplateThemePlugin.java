a99266e58727a380d0bddc9cb3e1d4a8da85bf0a
Adrien <adrien.crovetto@gmail.com>
2015-04-15 11:52:53 +0200

    add eTag support

--- src/play/modules/theme/TemplateThemePlugin.java
+++ src/play/modules/theme/TemplateThemePlugin.java
@@ -1,8 +1,21 @@
 package play.modules.theme;
 
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.CACHE_CONTROL;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.ETAG;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.IF_MODIFIED_SINCE;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.IF_NONE_MATCH;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.LAST_MODIFIED;
+
 import java.io.File;
+import java.text.ParseException;
+import java.util.Date;
 
 import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
+import org.jboss.netty.handler.codec.http.HttpMethod;
+import org.jboss.netty.handler.codec.http.HttpRequest;
+import org.jboss.netty.handler.codec.http.HttpResponse;
+import org.jboss.netty.handler.codec.http.HttpResponseStatus;
 
 import play.Logger;
 import play.Play;
@@ -14,6 +27,7 @@ import play.mvc.Http.Response;
 import play.mvc.Scope;
 import play.templates.Template;
 import play.templates.TemplateLoader;
+import play.utils.Utils;
 import play.vfs.VirtualFile;
 
 public class TemplateThemePlugin extends PlayPlugin {
@@ -99,6 +113,7 @@ public class TemplateThemePlugin extends PlayPlugin {
 		try {
 			byte[] bytes = VirtualFile.open(ctxFile).content();
 			String contentType = MimeTypes.getContentType(file.getName());
+			addEtag(request, response, file.getRealFile());
 			response.encoding = "UTF-8";
 			response.contentType = contentType;
 			response.setHeader("Content-Type", contentType);
@@ -110,6 +125,76 @@ public class TemplateThemePlugin extends PlayPlugin {
 			Logger.error("Erreur lors de serveStatic pour le path " + filePathWithCtx, e);
 			return super.serveStatic(file, request, response);
 		}
+	}
+
+	/**
+	 * Methode from Play Framework that is private and don't call when serveStatic overload
+	 * We do little change to adjust to the context, mainly HttpRequest -> Request and same for response
+	 */
+	private static Response addEtag(Request nettyRequest, Response httpResponse, File file) {
+		if (Play.mode == Play.Mode.DEV) {
+			httpResponse.setHeader(CACHE_CONTROL, "no-cache");
+		} else {
+			// Check if Cache-Control header is not set
+			if (httpResponse.getHeader(CACHE_CONTROL) == null) {
+				String maxAge = Play.configuration.getProperty("http.cacheControl", "3600");
+				if (maxAge.equals("0")) {
+					httpResponse.setHeader(CACHE_CONTROL, "no-cache");
+				} else {
+					httpResponse.setHeader(CACHE_CONTROL, "max-age=" + maxAge);
+				}
+			}
+		}
+		boolean useEtag = Play.configuration.getProperty("http.useETag", "true").equals("true");
+		long last = file.lastModified();
+		final String etag = "\"" + last + "-" + file.hashCode() + "\"";
+		if (!isModified(etag, last, nettyRequest)) {
+			if (nettyRequest.method.equals(HttpMethod.GET)) {
+				httpResponse.status = HttpResponseStatus.NOT_MODIFIED.getCode();
+			}
+			if (useEtag) {
+				httpResponse.setHeader(ETAG, etag);
+			}
+
+		} else {
+			httpResponse.setHeader(LAST_MODIFIED, Utils.getHttpDateFormatter().format(new Date(last)));
+			if (useEtag) {
+				httpResponse.setHeader(ETAG, etag);
+			}
+		}
+		return httpResponse;
+	}
+
+	/**
+	 * Methode from Play Framework that is private and don't call when serveStatic overload
+	 * We do little change to adjust to the context, mainly HttpRequest -> Request and same for response
+	 */
+	public static boolean isModified(String etag, long last, Request nettyRequest) {
+
+		if (nettyRequest.headers.containsKey(IF_NONE_MATCH)) {
+			final String browserEtag = nettyRequest.headers.get(IF_NONE_MATCH).value();
+			if (browserEtag.equals(etag)) {
+				return false;
+			}
+			return true;
+		}
+
+		if (nettyRequest.headers.containsKey(IF_MODIFIED_SINCE)) {
+			final String ifModifiedSince = nettyRequest.headers.get(IF_MODIFIED_SINCE).value();
+
+			if (!StringUtils.isEmpty(ifModifiedSince)) {
+				try {
+					Date browserDate = Utils.getHttpDateFormatter().parse(ifModifiedSince);
+					if (browserDate.getTime() >= last) {
+						return false;
+					}
+				} catch (ParseException ex) {
+					Logger.warn("Can't parse HTTP date", ex);
+				}
+				return true;
+			}
+		}
+		return true;
 	}
 
 	private static String getCtx(Request request) {
@@ -129,7 +214,7 @@ public class TemplateThemePlugin extends PlayPlugin {
 			return getFullUrlCtx(domain);
 		}
 	}
-
+	
 	private static String getFullUrlCtx(String domain) {
 		String listString = Play.configuration.getProperty("particeep.ctx.list");
 		String[] list = listString.split(",");
